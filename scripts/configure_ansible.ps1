bdd1ac5e1035fde0c65dd18c5a9c53b350601718
Clairo Dorneles <clairo@clairodorneles.cloud>
2024-06-08 17:50:06 +0200

    changed ps1 url

--- scripts/configure_ansible.ps1
+++ scripts/configure_ansible.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://aka.ms/sdaf-ansible"
+$url = "https://raw.githubusercontent.com/CGDORNELES/utilities/main/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
