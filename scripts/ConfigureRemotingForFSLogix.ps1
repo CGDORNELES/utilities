b96c4fdf888d56e9510ddea82b98c20ed74126a5
Clairo Dorneles <clairo@clairodorneles.cloud>
2024-12-17 08:36:17 -0300

    add automation for FSLogix

---
+++ scripts/ConfigureRemotingForFSLogix.ps1
@@ -0,0 +1,54 @@
+
+<#
+.SYNOPSIS
+Configures FSLogix settings for remoting.
+
+.DESCRIPTION
+This script configures various FSLogix settings by creating and setting registry keys and values. It ensures that FSLogix Profiles are enabled and configured with the specified settings.
+
+.PARAMETER profilefileShare
+The network location where VHD files will be stored.
+
+.EXAMPLE
+.\ConfigureRemotingForFSLogix.ps1 -profilefileShare "\\server\share"
+
+.NOTES
+Author: Your Name
+Date: Today's Date
+Version: 1.0
+#>
+param (
+  [string]$profilefileShare
+)
+
+# Create a new registry key for FSLogix under SOFTWARE, ignore if it already exists
+New-Item -Path "HKLM:\SOFTWARE" -Name "FSLogix" -ErrorAction Ignore
+# Create a new registry key for Profiles under FSLogix, ignore if it already exists
+New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -ErrorAction Ignore
+# Enable FSLogix Profiles
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value 1 -force
+# Set the location for VHD files
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value $profilefileShare -force
+# Allow concurrent user sessions
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ConcurrentUserSessions" -Value 1 -force
+# Delete the local profile when a VHD should be applied
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -force
+# Change the profile directory name on each login/logout
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value 1 -force
+# Enable dynamic VHD(X) disk type
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "IsDynamic" -Value 1 -force
+# Do not keep a local copy of the profile directory
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "KeepLocalDir" -Value 0 -force
+# Set the profile type to local
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "ProfileType" -Value 0 -force
+# Set the maximum size of the VHD(X) in MBs
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "SizeInMBs" -Value 15000 -force
+# Set the disk type to VHDX
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "VHDX" -force
+# Access the network as a computer object
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "AccessNetworkAsComputerObject" -Value 1 -force
+
+# Store credentials to access the storage account
+cmdkey.exe /add:$fileShare /user:$($useraccount) /pass:$($accesskey)
+# Disable Windows Defender Credential Guard (only needed for Windows 11 22H2)
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LsaCfgFlags" -Value 0 -force
--- scripts/configure_fslogix.ps1
+++ scripts/configure_fslogix.ps1
@@ -1,17 +1,34 @@
-configuration configureFSlogix {
+<#
+.SYNOPSIS
+Brief description of the function or script.
+#>
 
-    param
-    (
-        [Parameter(Mandatory = $true)]
-        [string]$storageAccountName
-    )
+.DESCRIPTION
+Detailed description of the function or script, including its purpose and functionality.
+This script downloads and executes a PowerShell script from a specified URL. The downloaded script is saved to a temporary location and then executed with specific parameters to configure remoting for FSLogix.
 
-    Node localhost {
+The script uses `System.Net.WebClient` to download the file and `powershell.exe` to execute it with the `-ExecutionPolicy ByPass` parameter to allow the script to run without being blocked by the execution policy. Additional parameters such as `-ForceNewSSLCert` and `-EnableCredSSP` are passed to the script to configure SSL certificates and Credential Security Support Provider (CredSSP) for remoting.
 
-        $regPath = "HKLM:\SOFTWARE\FSLogix\profiles"
-        New-ItemProperty -Path $regPath -Name Enabled -PropertyType DWORD -Value 1 -Force
-        New-ItemProperty -Path $regPath -Name VHDLocations -PropertyType MultiString -Value \\$storageAccountName.file.core.windows.net\fslogix -Force
-    
-    }
+.PARAMETER <ParameterName>
+Description of the parameter and its purpose.
 
-}
+.EXAMPLE
+Example of how to use the function or script.
+
+.NOTES
+Additional information about the function or script, such as author, date, and version.
+
+.LINK
+Related links or references.
+#>
+
+$url = "https://raw.githubusercontent.com/CGDORNELES/utilities/main/scripts/ConfigureRemotingForFSLogix.ps1"
+$file = "$env:temp\ConfigureRemotingForFSLogix.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file -ForceNewSSLCert -EnableCredSSP -Verbose
+
+Enable-WSManCredSSP -Role Server -Force
+
+Set-Item -Path "WSMan:\localhost\Service\Auth\CredSSP" -Value $true
